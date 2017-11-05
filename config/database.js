35d6a2f11579aadaad2d02c9364bdb0936081172
Rohit Kumawat <rkumawat125@gmail.com>
2017-11-05 21:21:41 +0530

    updated app

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost:27017/meanauth',
+  database: 'mongodb://Rohitkumawat1996:rohitk1761996@ds245885.mlab.com:45885/authapp',
   secret: 'yoursecret'
 }
